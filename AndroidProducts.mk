520eba1c217f4318fb80aa8323ea4f7d381c6bd1
yatto <yattodettaman@hotmail.com>
2019-11-26 18:48:21 +0100

    duma: switch from add_lunch_combo to COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_duma.mk 
+    $(LOCAL_DIR)/lineage_duma.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_duma-user \
+    lineage_duma-userdebug \
+    lineage_duma-eng
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
-add_lunch_combo lineage_duma-eng
-add_lunch_combo lineage_duma-user
-add_lunch_combo lineage_duma-userdebug
+# add_lunch_combo is deprecated
+
+# add_lunch_combo lineage_duma-eng
+# add_lunch_combo lineage_duma-user
+# add_lunch_combo lineage_duma-userdebug
+
