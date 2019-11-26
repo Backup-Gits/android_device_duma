99810dcac1b08fb318f3bfdc97803bd89aed80f7
yatto <yattodettaman@hotmail.com>
2019-11-26 19:15:28 +0100

    duma switch to passthrough for gnss

--- device.mk
+++ device.mk
@@ -179,8 +179,7 @@ PRODUCT_PACKAGES += \
 
 # GNSS HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service
+    android.hardware.gnss@1.0-impl
 
 # GPS
 PRODUCT_PACKAGES += \
